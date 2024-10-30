fbf9e2d7f4469be09dfb7e6ce33f25ce6dfa8c0a
CCU <ccu@corp.urpet96.ru>
2024-10-30 11:21:19 +0500

    Add all methods of CRUD operations

--- Program.cs
+++ Program.cs
@@ -1,26 +1,37 @@
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-List<Pumpkins> pumpkinsStores = new List<Pumpkins>
+List<Pumpkins> pumpkinsStore = new List<Pumpkins>
 {
-    new Pumpkins("Тыква Классическая", "Амазонка", "Classic scary carved Jack O’Lantern", "Ярко-оранжевая", 20, 700, 359)
+    new Pumpkins("Тыква Классическая", "Амазонка", "Classic scary carved Jack O’Lantern", "Ярко-оранжевая", 20, 700, 359),
+    new Pumpkins("Тыква с двойной головой", "Атлантик", "A two-headed monster pumpkin!", "Бледно-желтая", 40, 1500, 659),
+    new Pumpkins("Ужастная тыква", "Августина", "Nightmare pumpkin", "Оранжевая",28, 850, 499)
 };
+//Read
+app.MapGet("/", () => pumpkinsStore);
 
-app.MapGet("/", () => pumpkinsStores);
-
-app.MapPut("/{name}", (string name, PumpkinsDTO dto) =>
+//Create
+app.MapPost("/", (Pumpkins newPumpkin) =>
 {
-    Pumpkins buffer = pumpkinsStores.Find(p => p._pumpkinName == name);
-
-    buffer._pumpkinSize = dto.pumpkinSize == 0 ? buffer._pumpkinSize : dto.pumpkinSize;
-    buffer._pumpkinWeight = dto.pumpkinWeight == 0 ? buffer._pumpkinWeight : dto.pumpkinWeight;
-    buffer._pumpkinPrice = dto.pumpkinPrice == 0 ? buffer._pumpkinPrice : dto.pumpkinPrice;
+    pumpkinsStore.Add(newPumpkin);
+    return Results.Ok();
 });
 
+//Update
+app.MapPut("/{name}", (string name, PumpkinsDTO dto) =>
+{
+    Pumpkins buffer = pumpkinsStore.Find(p => p.pumpkinName == name);
+
+    buffer.pumpkinSize = dto.pumpkinSize == 0 ? buffer.pumpkinSize : dto.pumpkinSize;
+    buffer.pumpkinWeight = dto.pumpkinWeight == 0 ? buffer.pumpkinWeight : dto.pumpkinWeight;
+    buffer.pumpkinPrice = dto.pumpkinPrice == 0 ? buffer.pumpkinPrice : dto.pumpkinPrice;
+});
+
+//Delete
 app.MapDelete("/delete/{name}", (string name) =>
 {
-    Pumpkins buffer = pumpkinsStores.Find(p => p._pumpkinName == name);
-    pumpkinsStores.Remove(buffer);
+    Pumpkins buffer = pumpkinsStore.Find(p => p.pumpkinName == name);
+    pumpkinsStore.Remove(buffer);
 });
 
 app.Run();
@@ -31,20 +42,20 @@ class Pumpkins
 {
     public Pumpkins(string pumpkinName, string pumpkinVariety, string pumpkinFaceType, string pumpkinColor, float pumpkinSize, float pumpkinWeight, float pumpkinPrice)
     {
-        _pumpkinName = pumpkinName;
-        _pumpkinVariety = pumpkinVariety;
-        _pumpkinFaceType = pumpkinFaceType;
-        _pumpkinColor = pumpkinColor;
-        _pumpkinSize = pumpkinSize;
-        _pumpkinWeight = pumpkinWeight;
-        _pumpkinPrice = pumpkinPrice;
+        this.pumpkinName = pumpkinName;
+        this.pumpkinVariety = pumpkinVariety;
+        this.pumpkinFaceType = pumpkinFaceType;
+        this.pumpkinColor = pumpkinColor;
+        this.pumpkinSize = pumpkinSize;
+        this.pumpkinWeight = pumpkinWeight;
+        this.pumpkinPrice = pumpkinPrice;
     }
 
-    public string _pumpkinName { get; set; }
-    public string _pumpkinVariety { get; set; }
-    public string _pumpkinFaceType { get; set; }
-    public string _pumpkinColor { get; set; }
-    public float _pumpkinSize { get; set; }
-    public float _pumpkinWeight { get; set; }
-    public float _pumpkinPrice { get; set; }
+    public string pumpkinName { get; set; }
+    public string pumpkinVariety { get; set; }
+    public string pumpkinFaceType { get; set; }
+    public string pumpkinColor { get; set; }
+    public float pumpkinSize { get; set; }
+    public float pumpkinWeight { get; set; }
+    public float pumpkinPrice { get; set; }
 }
